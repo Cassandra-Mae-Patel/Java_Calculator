414c697df66148cdafeba2dcdfd930fb025d05a0
DESKTOP-8M6U7JG\Cassandra Mae <cassandramae99@gmail.com>
2021-02-08 19:12:19 -0500

    Circle
    Signed-off-by: <cassandramae99@gmail.com>

--- Calculator/src/application/Main.java
+++ Calculator/src/application/Main.java
@@ -3,19 +3,27 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		StackPane pane = new StackPane();
-		pane.getChildren().add(new Button("OK"));
-		Scene scene = new Scene(pane, 200,50);
-		primaryStage.setTitle("Button in a pane");
+		Circle circle = new Circle();
+		circle.setCenterX(100);
+		circle.setCenterY(100);
+		circle.setRadius(50);
+		circle.setStroke(Color.DARKRED);
+		circle.setFill(Color.CADETBLUE);
+		
+		Pane pane = new Pane();
+		pane.getChildren().add(circle);
+		
+		Scene scene = new Scene(pane,200,200);
+		primaryStage.setTitle("ShowCircle");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}

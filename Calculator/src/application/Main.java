e23a0eb9118666de2945c020c4724a290cbbe8b8
DESKTOP-8M6U7JG\Cassandra Mae <cassandramae99@gmail.com>
2021-02-08 21:33:05 -0500

    Centered Circle

--- Calculator/src/application/Main.java
+++ Calculator/src/application/Main.java
@@ -12,18 +12,18 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		Circle circle = new Circle();
-		circle.setCenterX(100);
-		circle.setCenterY(100);
-		circle.setRadius(50);
-		circle.setStroke(Color.DARKRED);
-		circle.setFill(Color.CADETBLUE);
-		
 		Pane pane = new Pane();
+		
+		Circle circle =  new Circle();
+		circle.centerXProperty().bind(pane.widthProperty().divide(2));
+		circle.centerYProperty().bind(pane.heightProperty().divide(2));
+		circle.setRadius(50);
+		circle.setStroke(Color.RED);
+		circle.setFill(Color.AQUAMARINE);
 		pane.getChildren().add(circle);
 		
-		Scene scene = new Scene(pane,200,200);
-		primaryStage.setTitle("ShowCircle");
+		Scene scene = new Scene(pane, 200,200);
+		primaryStage.setTitle("Centered Circle");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
